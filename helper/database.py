ba805acf4ede13c12b10dacc4aec565afe3d976e
Rohit <princeveg3ta732@gmail.com>
2026-03-26 08:27:51 +0530

    Database
    Renamed database reference and added metadata handling functions.

--- helper/database.py
+++ helper/database.py
@@ -6,15 +6,17 @@ class Database:
 
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
-        self.madflixbotz = self._client[database_name]
-        self.col = self.madflixbotz.user
+        self.codeifybots = self._client[database_name]   # ✅ renamed
+        self.col = self.codeifybots.user
 
     def new_user(self, id):
         return dict(
             _id=int(id),                                   
             file_id=None,
             caption=None,
-            format_template=None  # Add this line for the format template
+            format_template=None,
+            metadata=False,          # 🔥 added
+            metadata_code=""         # 🔥 added
         )
 
     async def add_user(self, b, m):
@@ -29,51 +31,57 @@ class Database:
         return bool(user)
 
     async def total_users_count(self):
-        count = await self.col.count_documents({})
-        return count
+        return await self.col.count_documents({})
 
     async def get_all_users(self):
-        all_users = self.col.find({})
-        return all_users
+        return self.col.find({})
 
     async def delete_user(self, user_id):
         await self.col.delete_many({'_id': int(user_id)})
     
     async def set_thumbnail(self, id, file_id):
-        await self.col.update_one({'_id': int(id)}, {'$set': {'file_id': file_id}})
+        await self.col.update_one({'_id': int(id)}, {'$set': {'file_id': file_id}}, upsert=True)
 
     async def get_thumbnail(self, id):
         user = await self.col.find_one({'_id': int(id)})
-        return user.get('file_id', None)
+        return user.get('file_id', None) if user else None
 
     async def set_caption(self, id, caption):
-        await self.col.update_one({'_id': int(id)}, {'$set': {'caption': caption}})
+        await self.col.update_one({'_id': int(id)}, {'$set': {'caption': caption}}, upsert=True)
 
     async def get_caption(self, id):
         user = await self.col.find_one({'_id': int(id)})
-        return user.get('caption', None)
+        return user.get('caption', None) if user else None
 
     async def set_format_template(self, id, format_template):
-        await self.col.update_one({'_id': int(id)}, {'$set': {'format_template': format_template}})
+        await self.col.update_one({'_id': int(id)}, {'$set': {'format_template': format_template}}, upsert=True)
 
     async def get_format_template(self, id):
         user = await self.col.find_one({'_id': int(id)})
-        return user.get('format_template', None)
+        return user.get('format_template', None) if user else None
         
     async def set_media_preference(self, id, media_type):
-        await self.col.update_one({'_id': int(id)}, {'$set': {'media_type': media_type}})
+        await self.col.update_one({'_id': int(id)}, {'$set': {'media_type': media_type}}, upsert=True)
         
     async def get_media_preference(self, id):
         user = await self.col.find_one({'_id': int(id)})
-        return user.get('media_type', None)
+        return user.get('media_type', None) if user else None
+
+    # 🔥 METADATA FUNCTIONS (IMPORTANT)
+    async def get_metadata(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return user.get('metadata', False) if user else False
+
+    async def set_metadata(self, id, value):
+        await self.col.update_one({'_id': int(id)}, {'$set': {'metadata': value}}, upsert=True)
+
+    async def get_metadata_code(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return user.get('metadata_code', "") if user else ""
+
+    async def set_metadata_code(self, id, code):
+        await self.col.update_one({'_id': int(id)}, {'$set': {'metadata_code': code}}, upsert=True)
 
 
-
-madflixbotz = Database(Config.DB_URL, Config.DB_NAME)
-        
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Developer @JishuDeveloper
+# ✅ FINAL OBJECT (IMPORTANT NAME)
+db = Database(Config.DB_URL, Config.DB_NAME)
